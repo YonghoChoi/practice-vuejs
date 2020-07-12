23bb6fa9e5d21da071f45e911e1801bff094d8a2
YonghoChoi <yongho1037@gmail.com>
2020-07-12 18:52:20 +0900

    favicon 추가 및 webpack build 설정 추가

--- fascampus/apps/todo/webpack.config.js
+++ fascampus/apps/todo/webpack.config.js
@@ -1,12 +1,14 @@
 // node.js 런타임에 실행됨. 아래는 node.js의 문법으로 작성
 const path = require('path') // 경로 지정을 돕기 위해 path 모듈 import
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-require('@babel/polyfill')  // import만 하고 사용은 하지 않기 때문에 변수 대입 없음
+const HtmlWebpackPlugin = require('html-webpack-plugin') // webpack 빌드 시 index.html을 dist로 추출하기 위한 플러그인
+// const CopyPlugin = require('copy-webpack-plugin') // favicon 파일을 webpack 빌드 시 복사하기 위한 플러그인 (windows에서 오류가 발생하여 주석)
+require('@babel/polyfill') // import만 하고 사용은 하지 않기 때문에 변수 대입 없음
 
 module.exports = {
     entry: { // webpack 실행 진입점. webpack으로 프로젝트 bundle 실행 시 가장 먼저 실행될 파일 지정
         app: [
-            '@babel/polyfill',  // 구 브라우저 호환이 필요한 경우 polyfill 지정
+            '@babel/polyfill', // 구 브라우저 호환이 필요한 경우 polyfill 지정
             path.join(__dirname, 'main.js') // __dirname은 현재 디렉토리 경로가 담긴 node.js에서 사용되는 전역변수
         ]
     },
@@ -15,8 +17,7 @@ module.exports = {
         path: path.join(__dirname, 'dist') // bundle 결과 파일들이 dist 디렉토리에 생성됨
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 // test 내용은 정규 표현식. 양슬래시 사이에 표현식이 들어감.
                 // 표현식 안에 dot(.)은 정규표현식에서 문자 하나로 인식되기 때문에 확장자의 .으로 인식시키기 위해 역슬래시 사용
                 // 문자열의 끝을 의미하는 $를 마지막에 입력
@@ -39,6 +40,16 @@ module.exports = {
         ]
     },
     plugins: [
-        new VueLoaderPlugin() // module이 실행 된 후 추가적인 처리를 Plugin이 담당
+        new VueLoaderPlugin(), // module이 실행 된 후 추가적인 처리를 Plugin이 담당
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "index.html")
+        }),
+         // from에 지정한 디렉토리 내 파일들을 to 경로에 복사 (output에 설정한 dist 디렉토리가 기본 디렉토리)
+        // new CopyPlugin({
+        //     patterns: [{
+        //         from: 'assets',
+        //         to: 'dist'
+        //     }],
+        // })
     ]
 }
