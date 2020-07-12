977c571c9be26eed60fd712a36516f500111974e
YonghoChoi <yongho1037@gmail.com>
2020-07-12 15:23:25 +0900

    Vue Loader 설정 추가

--- fascampus/apps/todo/README.md
+++ fascampus/apps/todo/README.md
@@ -11,6 +11,13 @@ or
 npm i -D webpack webpack-cli webpack-dev-server webpack-merge
 ```
   - webpack은 개발환경에서만 사용되기 때문에 --save-dev 옵션 지정
-4. webpack.config.js 파일 생성
-5. webpack.config.js 파일에 설정 입력
+4. webpack.config.js 파일 생성 및 설정
+5. webpack-cli 글로벌 설치
+```
+npm i -g webpack-cli
+```
+6. Vue Loader 설치
+```
+npm i -D vue-loader vue-template-compiler
+```
 
--- fascampus/apps/todo/webpack.config.js
+++ fascampus/apps/todo/webpack.config.js
@@ -1,14 +1,22 @@
 // node.js 런타임에 실행됨. 아래는 node.js의 문법으로 작성
-const path = require('path')    // 경로 지정을 돕기 위해 path 모듈 import
+const path = require('path') // 경로 지정을 돕기 위해 path 모듈 import
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     entry: { // webpack 실행 진입점. webpack으로 프로젝트 bundle 실행 시 가장 먼저 실행될 파일 지정
-        app: path.join(__dirname, 'main.js')    // __dirname은 현재 디렉토리 경로가 담긴 node.js에서 사용되는 전역변수
+        app: path.join(__dirname, 'main.js') // __dirname은 현재 디렉토리 경로가 담긴 node.js에서 사용되는 전역변수
     },
     output: { // webpack 실행 후 결과물에 대한 설정
         filename: '[name].js', // webpack 실행 결과물에 app.js 파일에 작성됨, [name]으로 지정하면 entry에 지정한 app 이름이 파일명으로 삽입됨
-        path: path.join(__dirname, 'dist')  // bundle 결과 파일들이 dist 디렉토리에 생성됨
+        path: path.join(__dirname, 'dist') // bundle 결과 파일들이 dist 디렉토리에 생성됨
     },
-    module: {},
-    plugins: []
+    module: {
+        rules: [{
+            test: /\.vue$/, // .vue 확장자를 가진 파일인 경우에는
+            loader: 'vue-loader' // vue-loader를 실행
+        }]
+    },
+    plugins: [
+        new VueLoaderPlugin()   // module이 실행 된 후 추가적인 처리를 Plugin이 담당
+    ]
 }
